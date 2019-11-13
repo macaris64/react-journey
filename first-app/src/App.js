dde51b4441c3b2c8eba1a2b387872e79c888e64f
Mehmet Acar <mehmetacar683@gmail.com>
2019-11-13 16:30:22 +0300

    create react first app

---
+++ first-app/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>src/App.js</code> 'yi düzenleyin ve kaydedin.
+        </p>
+        <a
+          className="App-link"
+          href="https://tr.reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
