700bedcd8f0cec09d9ff0c58b281e2b758be36f6
Mehmet Acar <mehmetacar683@gmail.com>
2019-11-13 21:27:36 +0300

    JSX Intro

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -2,12 +2,30 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-        <h1>Hi, I am React app !!</h1>
-        <small>Author -> Mehmet Acar</small>
-    </div>
-  );
+    return(
+        <div className='App'>
+            <h1>Hi, I am a React App !!</h1>
+        </div>
+    )
+    /*
+    return React.createElement( 'div',
+        {
+            className: 'App',
+            id: 'App',
+            otherProperty: 'other'
+        },
+        React.createElement(
+            'h1',
+            null,
+            React.createElement(
+                'p',
+                null,
+                'Hi, I\'m a React App'
+            )
+        )
+    );
+     */
+
 }
 
 export default App;
